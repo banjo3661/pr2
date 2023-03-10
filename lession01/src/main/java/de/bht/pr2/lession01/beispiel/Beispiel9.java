a820aced4ef456908fbc040ddcf719811b683315
banjo3661 <57414814+banjo3661@users.noreply.github.com>
2023-03-10 01:01:42 +0100

    lkl

--- lession01/src/main/java/de/bht/pr2/lession01/beispiel/Beispiel9.java
+++ lession01/src/main/java/de/bht/pr2/lession01/beispiel/Beispiel9.java
@@ -7,15 +7,17 @@ public class Beispiel9 {
 
   // Ungepruefte Ausnahme beim Compiler ignoriert
 
-  public static void dateiUndDivision(int zaehler, String dateiName) throws ArithmeticException, // Ungeprueft /* TODO: Nimm 'ArithmeticException' weg und schau, dass es noch kompiliert.
-      FileNotFoundException
-
-  { // Geprueft
+  public static void dateiUndDivision(int zaehler, String dateiName)
+      throws  // Ungeprueft /* TODO: Nimm 'ArithmeticException' weg und schau, dass es noch kompiliert.
+      FileNotFoundException , ArithmeticException{ // Geprueft
     int nenner = 0;
 
     /* TODO: Tausche die nächsten zwei Zeilen aus um den Effekt zu sehen, dass eine andere Exception zuerst geworfen wird. */
-    int division = zaehler / nenner;
+
+
     FileInputStream file = new FileInputStream(dateiName);
+    int division = zaehler / nenner;
+
   }
 
   public static void main(String[] args) {
